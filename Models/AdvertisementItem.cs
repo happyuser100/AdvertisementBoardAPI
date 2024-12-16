2c9ad61895423720bda3b0bb22045a35cc2fcc95
happyuser100 <fleidmos@gmail.com>
2024-12-16 09:47:46 +0200

    Add AdvertisementItem model

---
+++ Models/AdvertisementItem.cs
@@ -0,0 +1,17 @@
+﻿namespace api.Models
+{
+    public class AdvertisementItem
+    {
+        public Guid Id { get; set; }
+        public string Place { get; set; } = string.Empty;
+        public string AdProperty { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string IconUrl { get; set; } = string.Empty;
+        public string PersonName { get; set; } = string.Empty;
+        public DateTime PostDate { get; set; } = DateTime.Now;
+        public int CommentsNumber { get; set; } = 0;
+        public bool IsWriteComments { get; set; } = false;
+    }
+}
